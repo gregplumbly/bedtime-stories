8359f8e94b754e36d25dc38bd29de5cbf4f5e4ae
Greg Plumbly <greg.plumbly@gmail.com>
2023-05-19 10:09:48 +0100

    update about page.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,13 @@
 export default function Page() {
-  return <p>AI AI AI AI AI AI AI AI AI</p>;
+  return (
+    <div>
+      <p className="ml-8 mt-2">
+        I created this to learn a bit about using ChatGPT and the OpenAI API.
+      </p>
+      <p className="ml-8">
+        I shared it on a whatsapp parents group and was surprised to see how
+        much people enjoyed it. I hope you and your children enjoy it too.
+      </p>
+    </div>
+  );
 }
